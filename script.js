78f35bfe0c22b6ff025546fa106c4cb471f3daf2
shahrour <ahmadshahrour951@gmail.com>
2020-10-06 16:48:56 -0700

    Variables creation

---
+++ script.js
@@ -0,0 +1,15 @@
+// Define Variables
+const display = document.querySelector('#display');
+const dataOutput = document.querySelector('#data-output');
+
+const inputSize = document.querySelector('#input-size');
+const selectFont = document.querySelector('#select-font');
+const inputColor = document.querySelector('#input-color');
+const inputBgColor = document.querySelector('#input-bg-color');
+const inputEnterText = document.querySelector('#enter-text');
+const inputTextArea = document.querySelector('#enter-text');
+
+const showSize = document.querySelector('#show-size');
+const showFont = document.querySelector('#show-font');
+const showColor = document.querySelector('#show-color');
+const showBgColor = document.querySelector('#show-bg-color');
